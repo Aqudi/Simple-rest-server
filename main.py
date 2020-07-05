a44de62eaaf7e761b7a1cc5a19deb62ca1825dd6
Aqudi <gjdigj@naver.com>
2020-07-05 19:39:00 +0900

    Simple Naver search api example

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from fastapi import FastAPI
+
+import os
+import sys
+import requests
+
+from constants import client_id, client_secret
+
+app = FastAPI()
+
+BASE_URL = "https://openapi.naver.com/v1/search/blog?query="
+
+
+@app.get("/search")
+def search(keyword: str = "", display: int = 10, start: int = 1):
+    headers = {
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret
+    }
+    res = requests.get(url=BASE_URL+keyword, headers=headers)
+    json = res.json()
+
+    return {"result": json}
